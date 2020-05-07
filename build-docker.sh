74aeb1bc01f19864cb205eecc17082f41b433bd5
Vikas Dhiman <wecacuee@users.noreply.github.com>
2020-05-07 16:07:26 -0700

    update repositories

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
-docker build -f install-deps/Dockerfile_with_deps -t wecacuee/orcvio-backend-deps:latest --build-args BASE_IMG=wecacuee/orcvio-backend-deps:latest .
-docker build -f Dockerfile_with_deps -t orcvio-backend --build-args BASE_IMG=wecacuee/orcvio-backend-deps:latest .
+docker build -f install-deps/Dockerfile_with_deps -t wecacuee/orcvio-backend-deps:latest --build-arg BASE_IMG=wecacuee/orcvio-backend-deps:latest .
+docker build -f Dockerfile_with_deps -t orcvio-backend --build-arg BASE_IMG=wecacuee/orcvio-backend-deps:latest .
